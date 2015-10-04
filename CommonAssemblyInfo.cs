6af10d8535599dcf00975d55d666ea7e4427ae4d
Matt Owen <mowensoft@gmail.com>
2015-10-04 10:25:03 -0500

    Adding CommonAssemblyInfo

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("petejokes")]
+[assembly: AssemblyProduct("petejokes")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
